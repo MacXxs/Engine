db5512ea4b0f978be48aa99b264c939dbcce6892
Miquel Prieto <mikipmoline@gmail.com>
2022-10-13 21:33:16 +0200

    Add project files.

---
+++ ModuleRender.cpp
@@ -0,0 +1,95 @@
+#include "Globals.h"
+#include "Application.h"
+#include "ModuleRender.h"
+#include "ModuleWindow.h"
+#include "GL/glew.h"
+#include "SDL.h"
+
+ModuleRender::ModuleRender()
+{
+	SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 4); // desired version
+	SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 6);
+	SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_COMPATIBILITY);
+
+	SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1); // we want a double buffer
+	SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, 24); // we want to have a depth buffer with 24 bits
+	SDL_GL_SetAttribute(SDL_GL_STENCIL_SIZE, 8); // we want to have a stencil buffer with 8 bits
+}
+
+// Destructor
+ModuleRender::~ModuleRender()
+{
+}
+
+// Called before render is available
+bool ModuleRender::Init()
+{
+	LOG("Creating Renderer context");
+
+	SDL_Window* window = SDL_CreateWindow(
+		"SDL2/OpenGL Demo", 0, 0, 1920, 1080,
+		SDL_WINDOW_OPENGL | SDL_WINDOW_RESIZABLE);
+
+	this->context = SDL_GL_CreateContext(window);
+
+	GLenum err = glewInit();
+	// … check for errors
+	LOG("Using Glew %s", glewGetString(GLEW_VERSION));
+	// Should be 2.0
+
+	LOG("Vendor: %s", glGetString(GL_VENDOR));
+	LOG("Renderer: %s", glGetString(GL_RENDERER));
+	LOG("OpenGL version supported %s", glGetString(GL_VERSION));
+	LOG("GLSL: %s\n", glGetString(GL_SHADING_LANGUAGE_VERSION));
+
+	glEnable(GL_DEPTH_TEST); // Enable depth test
+	glEnable(GL_CULL_FACE); // Enable cull backward faces
+	glFrontFace(GL_CCW); // Front faces will be counter clockwise
+
+	return true;
+}
+
+update_status ModuleRender::PreUpdate()
+{
+	int *width, *height;
+	SDL_GetWindowSize(this->window, width, height);
+
+	glViewport(0, 0, (GLsizei)width, (GLsizei) height);
+
+	glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
+
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+	return UPDATE_CONTINUE;
+}
+
+// Called every draw update
+update_status ModuleRender::Update()
+{
+
+	return UPDATE_CONTINUE;
+}
+
+update_status ModuleRender::PostUpdate()
+{
+	SDL_GL_SwapWindow(this->window);
+	
+	return UPDATE_CONTINUE;
+}
+
+// Called before quitting
+bool ModuleRender::CleanUp()
+{
+	LOG("Destroying renderer");
+
+	SDL_GL_DeleteContext(this->context);
+
+	//Destroy window
+
+	return true;
+}
+
+void ModuleRender::WindowResized(unsigned width, unsigned height)
+{
+}
+
